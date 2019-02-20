f358721543aa7edf5a2e8078b685ac8900dc1496
OceansBlue <OceansBlueCode@gmail.com>
2019-02-20 02:14:55 -0600

    New Color Talking program

--- 001_theBasics.py
+++ 001_theBasics.py
@@ -8,3 +8,9 @@ while i < len(colors):
     print("When I was %d, my favorite color was %s" % (i, colors[i]))
     i = i+1
 
+colors = ['Yellow', 'Reed', 'Broooown', 'Hazellll']
+
+i = 0
+while i < len(colors):
+    print("When my age was %d, my BEST color WAS %s" % (i, colors[i]))
+    i = i+1
