c4fdf96f91b967197c30deecc1746821b6071c58
OceansBlue <33443506+OceansBlue2017@users.noreply.github.com>
2019-02-20 02:30:24 -0600

    Add files via upload

---
+++ 001_theBasics.py
@@ -0,0 +1,16 @@
+# First Line of Code to test
+
+
+colors = ['Blue', 'Red', 'Brown', 'Hazel']
+
+i = 0
+while i < len(colors):
+    print("When I was %d, my favorite color was %s" % (i, colors[i]))
+    i = i+1
+
+colors = ['Yellow', 'Reed', 'Broooown', 'Hazellll']
+
+i = 0
+while i < len(colors):
+    print("When my age was %d, my BEST color WAS %s" % (i, colors[i]))
+    i = i+1
