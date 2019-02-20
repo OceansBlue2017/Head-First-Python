513197328799933075bb4d104a042013e9cea624
OceansBlue <OceansBlueCode@gmail.com>
2019-02-20 01:29:09 -0600

    Test

--- 001_theBasics.py
+++ 001_theBasics.py
@@ -1,23 +1,2 @@
 # First Line of Code to test
 
-
-colors = ['Blue', 'Red', 'Brown', 'Hazel']
-
-i = 0
-while i < len(colors):
-    print("When I was %d, my favorite color was %s" % (i, colors[i]))
-    i = i+1
-
-colors = ['Yellow', 'Reed', 'Broooown', 'Hazellll']
-
-i = 0
-while i < len(colors):
-    print("When my age was %d, my BEST color WAS %s" % (i, colors[i]))
-    i = i+1
-
-colors = ['Blue', 'Red', 'Brown', 'Hazel']
-
-i = 0
-while i < len(colors):
-    print("When I was %d, my favorite color was %s" % (i, colors[i]))
-    i = i + 1
