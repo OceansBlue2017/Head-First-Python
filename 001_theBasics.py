855a6aa1bd5741f7dd6fe1815814e91a62733771
OceansBlue <OceansBlueCode@gmail.com>
2019-02-21 21:56:54 -0600

    Head First

--- 001_theBasics.py
+++ 001_theBasics.py
@@ -8,7 +8,7 @@ while i < len(colors):
 '''
 
 # **************    Head First Python Book  Page 4  Code    **************
-
+'''
 from datetime import datetime
 
 odds = [1,  3,  5,  7,  9,  11, 13, 15, 17, 19,
@@ -21,11 +21,11 @@ if right_this_minute in odds:
     print('This minute seems a little odd.')
 else:
     print('Not an odd minute.')
-
+'''
 # **************    Head First Python Book  Page  4  Code    **************
 
 # **************    Head First Python Book  Page 11  Code    **************
-
+'''
 import datetime
 Date = datetime.date.isoformat(datetime.date.today())
 print(Date)
@@ -39,11 +39,11 @@ htMl00 = html.escape('This HTML fragment contains a <script>script</script> tag.
 print(htMl00)
 htMl01 = html.escape("I &hearts; Python's &lt;standard library&gt;")
 print(htMl01)
-
+'''
 # **************    Head First Python Book  Page 11  Code    **************
 
 # **************    Head First Python Book  Page 17  Code    **************
-
+'''
 from datetime import datetime
 today = datetime.today()
 if today == 'Saturday':
@@ -52,15 +52,56 @@ elif today == 'Sunday':
     print('Recovery Day')
 else:
     print('Work, Work, and more Work')
-
+'''
 
 # **************    Head First Python Book  Page 17  Code    **************
 
-# **************    Head First Python Book  Page 20  Code    **************
+# **************    Head First Python Book  Page 39 Nook  Code    **************
+'''
+for s in [1, 2, 3,]:
+    print(s)
+
+for num in range(3):   #
+    print("it's alomst 3:00 AM")
+
+num = 0
+while num < 5:   #
+    print("it's alomst 15:00 AM Ha ha ha")
+    num = num +1
+    
+    '''
+# **************    Head First Python Book  Page 39 Nook  Code    **************
+
+# **************    Head First Python Book  Page 45 Nook Code    **************
+
+from datetime import datetime
+import random
+import time
+
+
+odds = [1,  3,  5,  7,  9,  11, 13, 15, 17, 19,
+       21, 23, 25, 27, 29,  31, 33, 35, 37, 39,
+       41, 43, 45, 47, 49,  51, 53, 55, 57, 59]
+
+for n in range(3):   #
+    right_this_minute = datetime.today().minute
+    waitTime = random.randint(1, 5)
+    time.sleep(waitTime)
+    if right_this_minute in odds:
+        print('This minute seems a little odd.  ' + 'and wait time was '+str(waitTime))
+    else:
+        print('Not an odd minute.  ' + 'And wait time was '+str(waitTime))
 
 
 
-# **************    Head First Python Book  Page 20  Code    **************
 
+"""
+num = 0
+while num < 5:   #
+    print("it's alomst 15:00 AM Ha ha ha")
+    num = num +1
+"""
+
+# **************    Head First Python Book  Page 45 Nook Code    **************
 
 
